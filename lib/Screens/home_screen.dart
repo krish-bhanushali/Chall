0c3ce77076393d22ea362c6c8f604e59bd8dcbb8
krish bhanushali <bhanushali123krish@gmail.com>
2020-09-02 17:04:03 +0530

    Creating on boarding screen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,13 +1,24 @@
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+
+class OnBoardingScreen extends StatefulWidget {
+  @override
+  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+ //Page Controller
+  PageController _pageController;
+
+  // keeping track of current page
+  int _currentPage = 0;
+
+  // isDarkTheme or not
+  bool _iseDarkTheme = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home screen'),
-      ),
-    );
+    return Container();
   }
 }
+
--- lib/resources/firebase_repository.dart
+++ lib/resources/firebase_repository.dart
@@ -1,9 +1,9 @@
-import 'package:chall/resources/firebase_methods.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-class FirebaseRepository {
-  FirebaseMethods firebaseMethods = FirebaseMethods();
-
-  Future<User> getCurrentUser() => firebaseMethods.getCurrentUser();
-  Future<UserCredential> signIn() => firebaseMethods.signInWithGoogle();
-}
+//import 'package:chall/resources/firebase_methods.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//
+//class FirebaseRepository {
+//  FirebaseMethods firebaseMethods = FirebaseMethods();
+//
+//  Future<User> getCurrentUser() => firebaseMethods.getCurrentUser();
+//  Future<UserCredential> signIn() => firebaseMethods.signInWithGoogle();
+//}
